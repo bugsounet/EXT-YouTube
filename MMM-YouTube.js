539e7c91b9ac4517593254e7566fc71014ab5555
bugsounet <github@bugsounet.fr>
2021-11-20 00:23:02 +0100

    little part of code...

--- MMM-YouTube.js
+++ MMM-YouTube.js
@@ -7,24 +7,32 @@
 // testing sample
 // @todo, make callback and remote controling
 
+logYT = (...args) => { /* do nothing */ }
+
 Module.register("MMM-YouTube", {
   defaults: {
     debug: false,
-    videoID: "Zi_XLOBDo_Y",
+    videoID: "sOnqjkJTMaA", //"Zi_XLOBDo_Y",
+    fullscreen: false,
     width: "800px",
     height: "600px",
   },
 
   start: function() {
-    console.log(this)
-    this.data.header = "\/!\\ @bugsounet YouTube Sub-Module Testing \/!\\",
-    this.data.position= "top_center"
+    if (this.config.fullscreen) this.data.position= "fullscreen_above"
+    else {
+      this.data.header = "\/!\\ @bugsounet YouTube Sub-Module Testing \/!\\"
+      this.data.position= "top_center"
+    }
+    if (this.config.debug) logYT = (...args) => { console.log("[YT]", ...args) }
     this.sendSocketNotification('INIT', this.config)
   },
 
   notificationReceived: function(notification, payload) {
     switch (notification) {
-      // this
+      case "DOM_OBJECTS_CREATED":
+        logYT("go")
+        break
     }
   },
 
@@ -37,12 +45,22 @@ Module.register("MMM-YouTube", {
   getDom: function() {
     var wrapper = document.createElement('div')
     wrapper.id = "YT_WINDOW"
-    wrapper.style.width = this.config.width
-    wrapper.style.height = this.config.height
-    var YT = document.createElement('iframe')
+    if (!this.config.fullscreen) {
+      wrapper.style.width = this.config.width
+      wrapper.style.height = this.config.height
+    }
+    var YT = document.createElement('webview')
     YT.id = "YT"
-    YT.src= "http://alexa.bugsounet.fr/test/test3.html?id="+this.config.videoID+"&origin="+ this.name + "&seed="+Date.now()
-    console.log(this.config.videoID+"&origin="+ this.name + "&seed="+Date.now())
+    YT.src= "http://youtube.bugsounet.fr/?id="+this.config.videoID+"&origin="+ this.name + "&seed="+Date.now()
+    YT.addEventListener("did-stop-loading", () => {
+      logYT("Video Loaded", YT.getURL())
+    })
+    YT.addEventListener("console-message", (message) => {
+      logYT(message)
+    })
+    YT.addEventListener("message", (message) => {
+      logYT("[Message]", message)
+    })
     wrapper.appendChild(YT)
     return wrapper
   },
@@ -51,5 +69,10 @@ Module.register("MMM-YouTube", {
     return [
       this.file('MMM-YouTube.css')
     ]
+  },
+
+  test: function () {
+    logYT("execute")
   }
+
 })
