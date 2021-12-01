2c8e09c29745314791913901b69c3f987fbc5afe
bugsounet <github@bugsounet.fr>
2021-12-01 22:00:18 +0100

    prepare installer

---
+++ installer/preinstall.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# +----------------+
+# | npm preinstall |
+# +----------------+
+
+# get the installer directory
+Installer_get_current_dir () {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+Installer_dir="$(Installer_get_current_dir)"
+
+# move to installler directory
+cd "$Installer_dir"
+
+# module name
+Installer_module="MMM-YouTube"
+
+echo
+
+# Let's start !
+Installer_info "Welcome to $Installer_module"
+
+echo
+
+# Check not run as root
+Installer_info "No root checking..."
+if [ "$EUID" -eq 0 ]; then
+  Installer_error "npm install must not be used as root"
+  exit 255
+fi
+Installer_chk "$(pwd)/../" "MMM-YouTube"
+Installer_chk "$(pwd)/../../../" "MagicMirror"
+echo
+
+# switch branch
+Installer_info "Installing Sources..."
+git checkout -f master 2>/dev/null || Installer_error "Installing Error !"
+git pull 2>/dev/null || Installer_error "Installing Error !"
+
+echo
+Installer_info "Installing all npm libraries..."
+cd ..
