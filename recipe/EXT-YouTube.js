3193a29750089488e6d187aadc197700fd3d8576
bugsounet <github@bugsounet.fr>
2022-02-06 03:55:16 +0100

    create recipe for GA v4

--- recipe/EXT-YouTube.js
+++ recipe/EXT-YouTube.js
@@ -1,38 +1,26 @@
-/** Vocal control for MMM-YouTube **/
+/** Vocal control for EXT-YouTube **/
 /**  @bugsounet  **/
-/** 24/11/2021 **/
+/** 06/02/2022 **/
 
 var recipe = {
   transcriptionHooks: {
     "YouTube": {
       pattern: "youtube (.*)",
       command: "YouTube_SEARCH"
-    },
-    "YouTube_STOP": {
-      pattern: "stop",
-      command: "YouTube_STOP"
-    },   
+    }
   },
 
   commands: {
     "YouTube_SEARCH": {
       functionExec: {
         exec: (params) => {
-          this.sendNotification("YT_SEARCH", params[1])
+          this.sendNotification("EXT_YOUTUBE-SEARCH", params[1])
         }
       },
       soundExec: {
         chime: "open",
       },
       displayResponse: true
-    },
-    "YouTube_STOP": {
-      notificationExec: {
-        notification: "YT_STOP"
-      },
-      soundExec: {
-        chime: "close"
-      }
     }
   }
 }
